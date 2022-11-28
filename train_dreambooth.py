b00ec1d4eb8e7b725ed6076d151b0ac05796e2a7
brian6091 <brian6091@gmail.com>
2022-11-28 16:06:53 +0100

    Allow list of prompts for saving

--- train_dreambooth.py
+++ train_dreambooth.py
@@ -87,7 +87,7 @@ def parse_args(input_args=None):
     )
     parser.add_argument(
         "--save_sample_prompt",
-        type=str,
+        type=list,
         default=None,
         help="The prompt used to generate sample outputs to save.",
     )
