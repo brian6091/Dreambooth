8d730828c9617d3340d1073eda0630654747ee7e
brian6091 <brian6091@gmail.com>
2022-12-12 22:18:37 +0100

    Update train_dreambooth.py

--- train_dreambooth.py
+++ train_dreambooth.py
@@ -772,6 +772,11 @@ def main(args):
             with open(os.path.join(save_dir, "args.json"), "w") as f:
                 json.dump(args.__dict__, f, indent=2)
                 
+            if args.train_text_encoder:
+                text_enc_model = accelerator.unwrap_model(text_encoder)
+            else:
+                text_enc_model = CLIPTextModel.from_pretrained(args.pretrained_model_name_or_path, subfolder="text_encoder", revision=args.revision)
+
             scheduler = DDIMScheduler(
                 beta_start=0.00085, 
                 beta_end=0.012, 
@@ -779,8 +784,25 @@ def main(args):
                 clip_sample=False, 
                 set_alpha_to_one=False,
                 steps_offset=1,
+            )            
+
+            pipeline = StableDiffusionPipeline.from_pretrained(
+                args.pretrained_model_name_or_path,
+                unet=accelerator.unwrap_model(
+                        ema_unet.averaged_model if args.use_ema else unet
+                    ),
+                text_encoder=text_enc_model,
+                vae=AutoencoderKL.from_pretrained(
+                    args.pretrained_vae_name_or_path or args.pretrained_model_name_or_path,
+                    subfolder=None if args.pretrained_vae_name_or_path else "vae",
+                    revision=None if args.pretrained_vae_name_or_path else args.revision,
+                ),
+                safety_checker=None,
+                scheduler=scheduler,
+                torch_dtype=torch.float16,
+                revision=args.revision,
             )
-                
+            
             if args.use_lora:
                 save_lora_weight(pipeline.unet, os.path.join(save_dir, "lora_unet.pt"))                
                 for _up, _down in extract_lora_ups_down(pipeline.unet):
@@ -793,6 +815,7 @@ def main(args):
                         target_replace_module=["CLIPAttention"],
                     )
                     
+                del pipeline
                 pipeline = StableDiffusionPipeline.from_pretrained(
                     args.pretrained_model_name_or_path,
                     text_encoder=CLIPTextModel.from_pretrained(
@@ -814,27 +837,6 @@ def main(args):
                 monkeypatch_lora(pipeline.text_encoder, os.path.join(save_dir, "lora_text_encoder.pt"), target_replace_module=["CLIPAttention"])
                 tune_lora_scale(pipeline.unet, 1.00)
             else:
-                if args.train_text_encoder:
-                    text_enc_model = accelerator.unwrap_model(text_encoder)
-                else:
-                    text_enc_model = CLIPTextModel.from_pretrained(args.pretrained_model_name_or_path, subfolder="text_encoder", revision=args.revision)
-
-                pipeline = StableDiffusionPipeline.from_pretrained(
-                    args.pretrained_model_name_or_path,
-                    unet=accelerator.unwrap_model(
-                            ema_unet.averaged_model if args.use_ema else unet
-                        ),
-                    text_encoder=text_enc_model,
-                    vae=AutoencoderKL.from_pretrained(
-                        args.pretrained_vae_name_or_path or args.pretrained_model_name_or_path,
-                        subfolder=None if args.pretrained_vae_name_or_path else "vae",
-                        revision=None if args.pretrained_vae_name_or_path else args.revision,
-                    ),
-                    safety_checker=None,
-                    scheduler=scheduler,
-                    torch_dtype=torch.float16,
-                    revision=args.revision,
-                )
                 pipeline.save_pretrained(save_dir)
 
             if args.save_sample_prompt is not None:
