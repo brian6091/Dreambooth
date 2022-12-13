bd70f522204b8cb57db9916bcee27cb530af0454
brian6091 <brian6091@gmail.com>
2022-12-13 23:14:48 +0100

    Update train_dreambooth.py

--- train_dreambooth.py
+++ train_dreambooth.py
@@ -992,7 +992,7 @@ def main(args):
                         if args.train_text_encoder
                         else unet.parameters()
                     )
-                    accelerator.clip_grad_norm_(params_to_clip, args.max_grad_norm)
+                    #accelerator.clip_grad_norm_(params_to_clip, args.max_grad_norm)
                 optimizer.step()
                 lr_scheduler.step()
                 if args.use_ema:
