5f0b9defa56389cff841c0be12923889b41983cb
brian6091 <brian6091@gmail.com>
2022-12-13 22:27:21 +0100

    Update train_dreambooth.py

--- train_dreambooth.py
+++ train_dreambooth.py
@@ -678,13 +678,13 @@ def main(args):
         print(summary(text_encoder, col_names=["num_params", "trainable"], verbose=1))
 
         with open(os.path.join(args.output_dir, "vae.txt"), "w") as f:
-            f.write(str(vae, col_names=["num_params", "trainable"], verbose=2))
+            f.write(str(summary(vae, col_names=["num_params", "trainable"], verbose=2)))
             f.close()
         with open(os.path.join(args.output_dir, "unet.txt"), "w") as f:
-            f.write(str(unet, col_names=["num_params", "trainable"], verbose=2))
+            f.write(str(summary(unet, col_names=["num_params", "trainable"], verbose=2)))
             f.close()
         with open(os.path.join(args.output_dir, "text_encoder.txt"), "w") as f:
-            f.write(str(text_encoder, col_names=["num_params", "trainable"], verbose=2))
+            f.write(str(summary(text_encoder, col_names=["num_params", "trainable"], verbose=2)))
             f.close()
         
     optimizer = optimizer_class(
