f0aaef481bda754e307140e38a8f3738992af249
brian6091 <brian6091@gmail.com>
2022-12-20 17:15:53 +0100

    Update datasets.py

--- utils/datasets.py
+++ utils/datasets.py
@@ -62,7 +62,7 @@ class FineTuningDataset(Dataset):
         self._length = self.num_instance_images
 
         self.instance_token = instance_token
-        self.instance_prompt = instance_prompt
+        self.instance_prompt = instance_prompt.format(instance_token)
         self.prompt_templates = prompt_templates
         
         if class_data_root is not None:
