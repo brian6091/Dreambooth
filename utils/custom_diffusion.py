804c98b067b37c56e5b4da005ce9d27a33dc0a47
brian6091 <brian6091@gmail.com>
2022-12-21 14:33:21 +0100

    Update custom_diffusion.py

--- utils/custom_diffusion.py
+++ utils/custom_diffusion.py
@@ -35,6 +35,8 @@
 # COMMERCIAL LOSS, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THE LICENSED MATERIALS, 
 # EVEN IF ADOBE HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
 
+from diffusers.models.attention import CrossAttention
+
 def unet_change_forward(unet):
 
     def new_forward(self, hidden_states, context=None, mask=None):
