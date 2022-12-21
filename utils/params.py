9ccedfec243533536ae8122eb95b14517fa7de8e
brian6091 <brian6091@gmail.com>
2022-12-21 21:58:52 +0100

    Update params.py

--- utils/params.py
+++ utils/params.py
@@ -93,7 +93,7 @@ def parse_args(input_args=None):
     )
     parser.add_argument(
         "--lora_text_modules",
-        nargs='+'
+        nargs='+',
         help="Modules of the text encoder to apply LoRA to.",
     )
     parser.add_argument(
