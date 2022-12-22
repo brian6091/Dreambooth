8a6b9aa53ac39f6b59de266d1feaf0a3112a09c4
brian6091 <brian6091@gmail.com>
2022-12-22 13:46:27 +0100

    Update finetune.py

--- finetune.py
+++ finetune.py
@@ -36,11 +36,11 @@ from lora_diffusion import (
     tune_lora_scale,
 )
 
-from utils.datasets import FineTuningDataset, PromptDataset
-from utils.textual_inversion_templates import object_templates, style_templates
-from utils.args import parse_args
-from utils.models import freeze_params, unfreeze_params
-from utils.utils import image_grid, get_full_repo_name, get_gpu_memory_map
+from src.datasets import FineTuningDataset, PromptDataset
+from src.textual_inversion_templates import object_templates, style_templates
+from src.args import parse_args
+from src.models import freeze_params, unfreeze_params
+from src.utils import image_grid, get_full_repo_name, get_gpu_memory_map
 
 logger = get_logger(__name__)
 
