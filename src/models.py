db0364260a3e6452e5c4b681df0f4fbeecfc7460
brian6091 <brian6091@gmail.com>
2022-12-22 21:32:29 +0100

    Update models.py

--- src/models.py
+++ src/models.py
@@ -7,3 +7,56 @@ def unfreeze_params(params):
     for param in params:
         param.requires_grad = True
         
+
+# Functions below here
+# MIT License
+
+# Copyright (c) 2021 Gido M. van de Ven
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+def count_parameters(model, verbose=True):
+    '''Count number of parameters, print to screen.'''
+    total_params = learnable_params = fixed_params = 0
+    for param in model.parameters():
+        n_params = index_dims = 0
+        for dim in param.size():
+            n_params = dim if index_dims==0 else n_params*dim
+            index_dims += 1
+        total_params += n_params
+        if param.requires_grad:
+            learnable_params += n_params
+        else:
+            fixed_params += n_params
+    if verbose:
+        print("--> this network has {} parameters (~{} million)"
+              .format(total_params, round(total_params / 1000000, 1)))
+        print("      of which: - learnable: {} (~{} million)".format(learnable_params,
+                                                                     round(learnable_params / 1000000, 1)))
+        print("                - fixed: {} (~{} million)".format(fixed_params, round(fixed_params / 1000000, 1)))
+    return total_params, learnable_params, fixed_params
+
+
+def print_model_info(model, title="MODEL"):
+    '''Print information on [model] onto the screen.'''
+    print("\n" + 40*"-" + title + 40*"-")
+    print(model)
+    print(90*"-")
+    _ = count_parameters(model)
+    print(90*"-")
