58af8f09dbc463c1126b0eca4ace0431ae2f5892
brian6091 <brian6091@gmail.com>
2022-12-26 09:45:58 +0100

    Update finetune.py

--- finetune.py
+++ finetune.py
@@ -647,7 +647,7 @@ def main(args):
                 noisy_latents = noise_scheduler.add_noise(latents, noise, timesteps)
 
                 # Get the text embedding for conditioning
-                encoder_hidden_states = text_encoder(batch["input_ids"])[0]
+                encoder_hidden_states = text_encoder(batch["input_ids"])[0].to(dtype=weight_dtype)
 
                 # Predict the noise residual
                 model_pred = unet(noisy_latents, timesteps, encoder_hidden_states).sample
