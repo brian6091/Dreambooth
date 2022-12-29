62fab3923b2cc169b754777acad95200fffebafd
brian6091 <brian6091@gmail.com>
2022-12-29 19:41:51 +0100

    Update optim.py

--- src/optim.py
+++ src/optim.py
@@ -12,7 +12,13 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 #
+import math
+#from enum import Enum
+#from typing import Optional, Union
+
 import torch
+from torch.optim import Optimizer
+from torch.optim.lr_scheduler import LambdaLR
 
 def load_optimizer(optname):    
     opts = dict()
@@ -126,3 +132,35 @@ def group_parameters(unet,
         
     return train_token_embedding, train_text_encoder, train_unet, params_to_optimize
     
+    
+
+def get_pivotal_tuning_schedule_with_warmup(
+    optimizer: Optimizer, num_warmup_steps: int, num_training_steps: int, num_cycles: float = 0.5, last_epoch: int = -1
+):
+    """
+    Create a schedule with a learning rate that decreases following the values of the cosine function between the
+    initial lr set in the optimizer to 0, after a warmup period during which it increases linearly between 0 and the
+    initial lr set in the optimizer.
+    Args:
+        optimizer ([`~torch.optim.Optimizer`]):
+            The optimizer for which to schedule the learning rate.
+        num_warmup_steps (`int`):
+            The number of steps for the warmup phase.
+        num_training_steps (`int`):
+            The total number of training steps.
+        num_periods (`float`, *optional*, defaults to 0.5):
+            The number of periods of the cosine function in a schedule (the default is to just decrease from the max
+            value to 0 following a half-cosine).
+        last_epoch (`int`, *optional*, defaults to -1):
+            The index of the last epoch when resuming training.
+    Return:
+        `torch.optim.lr_scheduler.LambdaLR` with the appropriate schedule.
+    """
+
+    def lr_lambda(current_step):
+        if current_step < num_warmup_steps:
+            return float(current_step) / float(max(1, num_warmup_steps))
+        progress = float(current_step - num_warmup_steps) / float(max(1, num_training_steps - num_warmup_steps))
+        return max(0.0, 0.5 * (1.0 + math.cos(math.pi * float(num_cycles) * 2.0 * progress)))
+    
+    return LambdaLR(optimizer, lr_lambda, last_epoch)
