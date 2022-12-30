a89f0294d28d1865348029e4e48b6f230bf6a398
brian6091 <brian6091@gmail.com>
2022-12-30 07:50:04 +0100

    Update finetune.py

--- finetune.py
+++ finetune.py
@@ -370,6 +370,7 @@ def main(args):
             warmup_steps=args.lr_warmup_steps * args.gradient_accumulation_steps,
             total_steps=args.max_train_steps * args.gradient_accumulation_steps,
             inversion_fraction=args.lr_inversion_fraction,
+            overlap_fraction=args.lr_overlap_fraction,
             explore_fraction0=0.5,
             explore_fraction1=0.5,
         )
