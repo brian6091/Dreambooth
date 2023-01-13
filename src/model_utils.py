a7a9a64a630080a860e8a0b1dcff35c6b80eabde
brian6091 <brian6091@gmail.com>
2023-01-13 09:02:03 +0100

    Update model_utils.py

--- src/model_utils.py
+++ src/model_utils.py
@@ -521,8 +521,7 @@ def save_trainable_parameters(
         if instance_token:
             # instance_token added to tokenizer, but all the other embeds are frozen.
             # The embedding will thus have requires_grad=TRUE, but we do not want to save it
-            td_text, md_text = get_trainable_param_dict(text_encoder)
-#             td_text, md_text = get_trainable_param_dict(text_encoder, exclude_params = {"token_embedding.weight"})
+            td_text, md_text = get_trainable_param_dict(text_encoder, exclude_params = {"token_embedding.weight"})
         else:
             td_text, md_text = get_trainable_param_dict(text_encoder)
             
