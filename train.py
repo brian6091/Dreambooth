20a0b5ad4168877cfda0875dfa2c6d04b32dddc7
brian6091 <brian6091@gmail.com>
2023-01-14 12:24:36 +0100

    REM: unnecessary monkeypatching

--- train.py
+++ train.py
@@ -970,9 +970,7 @@ def main(args):
                     torch_dtype=torch.float16,
                     revision=args.revision,
                 )
-                monkeypatch_lora(pipeline.unet, torch.load(os.path.join(save_dir, "lora_unet.pt")))
-                monkeypatch_lora(pipeline.text_encoder, torch.load(os.path.join(save_dir, "lora_text_encoder.pt")), target_replace_module=["CLIPAttention"])
-                tune_lora_scale(pipeline.unet, 1.00)
+                #tune_lora_scale(pipeline.unet, 1.00)
             else:
                 pipeline.save_pretrained(save_dir)
 
