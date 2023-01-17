218ccf7fe2db575042b9b9ca8c5c247c46de18c7
brian6091 <brian6091@gmail.com>
2023-01-17 08:19:15 +0100

    Update tracking.py

--- src/tracking.py
+++ src/tracking.py
@@ -2,8 +2,6 @@ import os
 import torch
 from tqdm.auto import tqdm
 
-#import wandb
-
 from accelerate.utils import (
     LoggerType,
     is_aim_available,
@@ -27,19 +25,15 @@ def get_intermediate_samples(
     sample_seed,
     save_n_sample,
     save_dir,
-    #sample_to_tracker, # remove this, rely on tracker !=none
     tracker,
     data_table,
     step,
-    # save_locally
 ):
     sample_prompt = sample_prompt.replace("{}", instance_token)
     sample_prompt = list(map(str.strip, sample_prompt.split('//')))
 
     g_cuda = torch.Generator(device=accelerator.device).manual_seed(sample_seed)
     pipeline.set_progress_bar_config(disable=True)
-    #sample_dir = os.path.join(save_dir, "samples")
-    #os.makedirs(sample_dir, exist_ok=True)
 
     with torch.autocast("cuda"), torch.inference_mode():
         all_images = []
@@ -56,14 +50,15 @@ def get_intermediate_samples(
             if data_table and tracker=="wandb" and is_wandb_available():
                 from wandb import Image
                 for prompt_id, im in enumerate(images):
-                    data_table.add_data(step, prompt_id, sample_prompt[prompt_id], sample_guidance_scale,
-                                       sample_seed, sample_id, Image(im))
+                    data_table.add_data(
+                        step,
+                        prompt_id,
+                        sample_prompt[prompt_id],
+                        sample_guidance_scale,
+                        sample_seed, sample_id,
+                        Image(im, caption=f"step:{step}, prompt_id:{prompt_id}, sample_id:{sample_id}")
+                       )
 
         grid = image_grid(all_images, rows=save_n_sample, cols=len(sample_prompt))
         
-#         if tracker=="wandb" and is_wandb_available():
-#             accelerator.log({"sample_grid":[wandb.Image(grid, caption="test")]}, step=step)
-        
-        #grid.save(os.path.join(sample_dir, f"{step}.jpg"), quality=90, optimize=True)
-
     return grid, data_table
