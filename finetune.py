384c03b07d26a0738b01f45fcabbbd0e7a07153d
brian6091 <brian6091@gmail.com>
2023-01-18 18:53:43 +0100

    Update finetune.py

--- finetune.py
+++ finetune.py
@@ -652,7 +652,6 @@ def main(args):
                             args.pretrained_vae_name_or_path or args.pretrained_model_name_or_path,
                             subfolder=None if args.pretrained_vae_name_or_path else "vae",
                             revision=None if args.pretrained_vae_name_or_path else args.revision,
-                            torch_dtype=torch_dtype,
                         ),
                         tokenizer=tokenizer,
                         text_encoder=accelerator.unwrap_model(text_encoder, **unwrap_args) \
