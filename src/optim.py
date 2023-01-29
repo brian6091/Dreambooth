abdcb277b9543005fe1a4cf597201dfe7ccab46f
brian6091 <brian6091@gmail.com>
2023-01-29 01:03:53 +0100

    Update optim.py

--- src/optim.py
+++ src/optim.py
@@ -74,7 +74,7 @@ def calculate_loss(input, target, loss_function="mse", loss_adjust=None, reducti
 def get_optimizer(optname):    
     opts = dict()
     
-    opts['SPS'] = SPS
+    #opts['SPS'] = SPS
     try:
         import bitsandbytes as bnb
         
